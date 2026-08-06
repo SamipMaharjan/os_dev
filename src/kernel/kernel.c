ab71f506ae8a0ec808c3f799ad758149cd619a67
SamipMaharjan <adsrb15@gmail.com>
2026-08-06 13:58:24 +0545

    serial port output test

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,115 +1,47 @@
-// #include <stdbool.h>
-#include <stddef.h>
+#define COM1 0x3F8
+
 #include <stdint.h>
 
-/* Check if the compiler thinks you are targeting the wrong operating system.  \
- */
-#if defined(__linux__)
-#error "You are not using a cross-compiler, you will most certainly run into
-trouble "
-#endif
-
-/* This tutorial will only work for the 32-bit ix86 targets. */
-#if !defined(__i386__)
-#error "This tutorial needs to be compiled with a ix86-elf compiler"
-#endif
-
-    /* Hardware text mode color constants. */
-    enum vga_color {
-      VGA_COLOR_BLACK = 0,
-      VGA_COLOR_BLUE = 1,
-      VGA_COLOR_GREEN = 2,
-      VGA_COLOR_CYAN = 3,
-      VGA_COLOR_RED = 4,
-      VGA_COLOR_MAGENTA = 5,
-      VGA_COLOR_BROWN = 6,
-      VGA_COLOR_LIGHT_GREY = 7,
-      VGA_COLOR_DARK_GREY = 8,
-      VGA_COLOR_LIGHT_BLUE = 9,
-      VGA_COLOR_LIGHT_GREEN = 10,
-      VGA_COLOR_LIGHT_CYAN = 11,
-      VGA_COLOR_LIGHT_RED = 12,
-      VGA_COLOR_LIGHT_MAGENTA = 13,
-      VGA_COLOR_LIGHT_BROWN = 14,
-      VGA_COLOR_WHITE = 15,
-    };
-
-static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) {
-  return fg | bg << 4;
+static inline void outb(uint16_t port, uint8_t value) {
+  __asm__ volatile("outb %0, %1" : : "a"(value), "Nd"(port));
 }
 
-static inline uint16_t vga_entry(unsigned char uc, uint8_t color) {
-  return (uint16_t)uc | (uint16_t)color << 8;
+static inline uint8_t inb(uint16_t port) {
+  uint8_t ret;
+  __asm__ volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
+  return ret;
 }
 
-size_t strlen(const char *str) {
-  size_t len = 0;
-  while (str[len])
-    len++;
-  return len;
+void serial_init(void) {
+  outb(COM1 + 1, 0x00); // Disable interrupts
+  outb(COM1 + 3, 0x80); // Enable DLAB
+  outb(COM1 + 0, 0x03); // Baud divisor low byte (38400 baud)
+  outb(COM1 + 1, 0x00); // Baud divisor high byte
+  outb(COM1 + 3, 0x03); // 8 bits, no parity, one stop bit
+  outb(COM1 + 2, 0xC7); // Enable FIFO
+  outb(COM1 + 4, 0x0B); // IRQs enabled, RTS/DSR set
 }
 
-#define VGA_WIDTH 80
-#define VGA_HEIGHT 25
-#define VGA_MEMORY 0xB8000
+int serial_is_transmit_empty(void) { return inb(COM1 + 5) & 0x20; }
 
-uint8_t END_KERNEL[7] =
-    "ELF END"; // just for spotting end of executable in hex editors and memory
+void serial_putchar(char c) {
+  while (!serial_is_transmit_empty())
+    ;
 
-size_t terminal_row;
-size_t terminal_column;
-uint8_t terminal_color;
-uint16_t *terminal_buffer = (uint16_t *)VGA_MEMORY;
-// uint16_t *terminal_buffer = (uint16_t *)0xB8000;
+  outb(COM1, c);
+}
+void serial_write(const char *str) {
+  while (*str) {
+    if (*str == '\n')
+      serial_putchar('\r');
 
-void breakpoint() { __asm__ volatile("xchg %bx, %bx"); }
-
-void terminal_initialize(void) {
-  // uint16_t *local_terminal_buffer = (uint16_t *)VGA_MEMORY;
-  terminal_row = 0;
-  terminal_column = 0;
-  terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-
-  for (size_t y = 0; y < VGA_HEIGHT; y++) {
-    for (size_t x = 0; x < VGA_WIDTH; x++) {
-      const size_t index = y * VGA_WIDTH + x;
-      terminal_buffer[index] = vga_entry(' ', terminal_color);
-    }
+    serial_putchar(*str++);
   }
-}
-
-void terminal_setcolor(uint8_t color) { terminal_color = color; }
-
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
-  if (c == '\n') {
-    terminal_row++;
-    terminal_column = 0;
-  }
-  size_t index = y * VGA_WIDTH + x;
-  terminal_buffer[index] = vga_entry(c, color);
-}
-
-void terminal_putchar(char c) {
-  terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-  if (++terminal_column == VGA_WIDTH) {
-    terminal_column = 0;
-    if (++terminal_row == VGA_HEIGHT)
-      terminal_row = 0;
-  }
-}
-
-void terminal_write(const char *data, size_t size) {
-  for (size_t i = 0; i < size; i++)
-    terminal_putchar(data[i]);
-}
-
-void terminal_writestring(const char *data) {
-  terminal_write(data, strlen(data));
 }
 
 void kernel_main(void) {
-  terminal_initialize();
-  terminal_writestring("Hello, kernel World!\n");
-  terminal_writestring("hehe Hello, kernel World!\n");
-  terminal_writestring("hehe 2Hello, kernel World!\n");
+  serial_init();
+  serial_write("Hello, kernel World!\n");
+  serial_write("hehe Hello, kernel World!\n");
+  serial_write("hehe 2Hello, kernel World!\n");
 }
