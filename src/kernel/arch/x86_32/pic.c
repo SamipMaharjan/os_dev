622a006659b3b8de801c0ba3a63a8777c2286353
SamipMaharjan <adsrb15@gmail.com>
2026-08-28 23:51:11 +0545

    PIC: PIC initialize fn

--- src/kernel/arch/x86_32/pic.c
+++ src/kernel/arch/x86_32/pic.c
@@ -9,10 +9,49 @@
 #define PIC2_COMMAND PIC2
 #define PIC2_DATA (PIC2 + 1)
 #define PIC_EOI 0x20 /* End-of-interrupt command code */
+/* reinitialize the PIC controllers, giving them specified vector offsets
+   rather than 8h and 70h, as configured by default */
+
+#define ICW1_ICW4 0x01      /* Indicates that ICW4 will be present */
+#define ICW1_SINGLE 0x02    /* Single (cascade) mode */
+#define ICW1_INTERVAL4 0x04 /* Call address interval 4 (8) */
+#define ICW1_LEVEL 0x08     /* Level triggered (edge) mode */
+#define ICW1_INIT 0x10      /* Initialization - required! */
+
+#define ICW4_8086 0x01       /* 8086/88 (MCS-80/85) mode */
+#define ICW4_AUTO 0x02       /* Auto (normal) EOI */
+#define ICW4_BUF_SLAVE 0x08  /* Buffered mode/slave */
+#define ICW4_BUF_MASTER 0x0C /* Buffered mode/master */
+#define ICW4_SFNM 0x10       /* Special fully nested (not) */
+
+#define CASCADE_IRQ 2
 
 void PIC_sendEOI(uint8_t irq) {
   if (irq >= 8)
     outb(PIC2_COMMAND, PIC_EOI);
 
   outb(PIC1_COMMAND, PIC_EOI);
+}
+void PIC_remap(int offset1, int offset2) {
+  outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);
+  io_wait();
+  outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+  io_wait();
+  outb(PIC1_DATA, offset1);
+  io_wait();
+  outb(PIC2_DATA, offset2);
+  io_wait();
+  outb(PIC1_DATA, 1 << CASCADE_IRQ);
+
+  io_wait();
+  outb(PIC2_DATA, CASCADE_IRQ);
+  io_wait();
+
+  outb(PIC1_DATA, ICW4_8086);
+  io_wait();
+  outb(PIC2_DATA, ICW4_8086);
+  io_wait();
+
+  outb(PIC1_DATA, 0);
+  outb(PIC2_DATA, 0);
 }
