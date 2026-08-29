2fb6f05e7234e5830c21f9960f6ec25c748a5b6b
SamipMaharjan <adsrb15@gmail.com>
2026-08-29 16:55:38 +0545

    GDB: config for debugging with gdb.

--- err_finder.sh
+++ err_finder.sh
@@ -1,6 +1,7 @@
 # while true 
 # do
 # tail -f serial.log
+cat serial.log
 echo -e "\n\n\n===============ERROR DETAILS============"
 err_addr=$(grep -oP "(?<=ERROR ADDRESS:).*" serial.log)
 i686-elf-addr2line -e ./build/kernel.elf -f -C $err_addr 
